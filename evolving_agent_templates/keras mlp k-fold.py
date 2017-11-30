6f93f488c58380ec08d80384d32ead61b6620272
Aichoo AIOS <ds@eghamtech.com>
2017-11-30 22:27:40 +0000

    Update keras mlp k-fold.py

--- evolving_agent_templates/keras mlp k-fold.py
+++ evolving_agent_templates/keras mlp k-fold.py
@@ -1,3 +1,8 @@
+# AICHOO OS Evolving Agent 
+# Documentation about AIOS and how to create Evolving Agents can be found on our WiKi
+# https://github.com/eghamtech/AIOS/wiki/Evolving-Agents
+# https://github.com/eghamtech/AIOS/wiki/AI-OS-Introduction
+
 #start_of_genes_definitions
 #key=fields_to_use;   type=random_int;	from=40;	to=58;	step=1
 #key=data;            type=random_array_of_fields;	length=58
@@ -43,4 +48,70 @@ tf.set_random_seed(1234)
 
 sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
 K.set_session(sess)
+
+
+import pandas as pd
+import math
+
+# obtain a unique ID for the current instance
+result_id = {id}
+# create new field name based on "field_ev_prefix" (also specified in Constants) with unique instance ID
+# and filename to save new field data
+field_ev_prefix = "ev_field_"
+output_column = field_ev_prefix + str(result_id)
+output_filename = output_column + ".csv"
+
+# obtain random field which will be the prediction target of this instance
+target_definition = "{source_field}"
+# field definition received from the kernel contains two parts: name of the field and CSV filename that holds the actual data
+# load these two parts into variables
+target_col = target_definition.split("|")[0]
+target_file = target_definition.split("|")[1]
+
+# obtain random selection of fields; number of fields to be selected specified in {data}:length gene for this instance
+data_defs = {data}
+
+# remove the target field for this instance from the data used for training
+if target_definition in data_defs:
+    data_defs.remove(target_definition)
+
+#############################################################
+#
+#                   DATA PREPARATION
+#
+#############################################################
+
+# read data from the original data file loaded into Memory (specified in Constants as "trainfile")
+# "workdir" must be specified in Constants - it is a global setting where all CSV files are stored on Jupyter server
+main_data = pd.read_csv(workdir+trainfile)
+# read data from CSV file containing the prediction target field selected for this instance
+df = pd.read_csv(workdir+target_file)[[target_col]]
+
+# read each required field's data from a corresponding CSV file
+# number of fields actually read specified in {fields_to_use} gene
+cols_count = 0
+for i in range(0,len(data_defs)):
+    cols_count+=1
+    if cols_count>{fields_to_use}:
+        break
+    col_name = data_defs[i].split("|")[0]
+    file_name = data_defs[i].split("|")[1]
+    
+    if file_name==trainfile:
+        df[col_name] = main_data[col_name]
+    else:
+        # read column from another CSV file and add to df
+        df = df.merge(pd.read_csv(workdir+file_name)[[col_name]], left_index=True, right_index=True)
+
+print ("data loaded", len(df), "rows; ", len(df.columns), "columns")
+
+# analyse target column whether it is binary which may result in different loss function used
+is_binary = df.sort_values(target_col)[target_col].unique().tolist()==[0, 1]
+if is_binary:
+    print ("detected binary target. use LOGLOSS")
+    # param = {'max_depth':{max_depth}, 'eta':{eta}, 'colsample_bytree':{colsample_bytree}, 'subsample': {subsample}, 'objective':'binary:logistic', 'eval_metric':'logloss', 'nthread':4}
+else:
+    print ("use MAE")
+    # param = {'max_depth':{max_depth}, 'eta':{eta}, 'colsample_bytree':{colsample_bytree}, 'subsample': {subsample}, 'objective':'reg:linear', 'eval_metric':'mae', 'nthread':4}
+
 
