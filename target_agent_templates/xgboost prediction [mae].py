fe4e69d333fa8d18b9702ed20d0dd61dffe84db0
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-05 21:03:01 +0300

    filter NaN targets

--- target_agent_templates/xgboost prediction [mae].py
+++ target_agent_templates/xgboost prediction [mae].py
@@ -42,8 +42,10 @@ print ("data loaded", len(df), "rows; ", len(df.columns), "columns")
 param = {'max_depth':{max_depth}, 'eta':{eta}, 'colsample_bytree':{colsample_bytree}, 'subsample': {subsample}, 'objective':'reg:linear', 'eval_metric':'mae', 'nthread':4}
 
 x_test = df.copy(deep=True).iloc[1::2]
+x_test = x_test[x_test[target].notnull()]
 x_test.reset_index(drop=True, inplace=True)
 x_train = df.copy(deep=True).iloc[0::2]
+x_train = x_train[x_train[target].notnull()]
 x_train.reset_index(drop=True, inplace=True)
 
 print ("train " + target + " mean:", x_train[target].mean())
