3206458f296575cda885352719c4d21a758b4532
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-06 14:11:33 +0300

    row_count

--- simple_agent_templates/Math/invert.py
+++ simple_agent_templates/Math/invert.py
@@ -16,5 +16,5 @@ df[output_column] = 1/df[col1]
 df[[output_column]].to_csv(workdir+output_filename)
 
 print ("1/" + col1)
-print ("#add_field:"+output_column+",N,"+output_filename)
+print ("#add_field:"+output_column+",N,"+output_filename+","+str(len(df)))
 
