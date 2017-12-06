be18d9fef76bd352b6fef84fc3eb29c32817716a
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-06 19:41:43 +0300

    simple output template

---
+++ output_agent_templates/simple_submission.py
@@ -0,0 +1,21 @@
+#start_of_parameters
+#key=DNA_ID;  type=constant;  value=12345
+#key=outfile;  type=constant;  value=submission.csv
+#key=default_num_round;  type=constant;  value=1000
+#end_of_parameters
+
+
+import pandas as pd
+
+default_num_round = {default_num_round}
+
+{run_dna {DNA_ID}}
+
+df = pd.read_csv(workdir+trainfile)
+df = df[df[target].isnull()]
+df.reset_index(drop=True, inplace=True)
+df[target] = prediction
+
+df.to_csv(workdir+outfile, index=False)
+
+print ("Output agent finished")
