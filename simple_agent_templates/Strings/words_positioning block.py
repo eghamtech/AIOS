c51a179d6ccdbb38a40458df7e0f67ca5c60f26f
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-06 14:20:03 +0300

    row_count

--- simple_agent_templates/Strings/words_positioning block.py
+++ simple_agent_templates/Strings/words_positioning block.py
@@ -413,8 +413,9 @@ for index, row in df.iterrows():
 
 total_cols = 18+(nfirst+nlast+nall+nextra)*2
 
+nrow = len(df)
 for i in range(1,total_cols+1):
     fld = fldprefix + '_' + str(i)
     fname = fld + '.csv'
     df[[fld]].to_csv(workdir+fname)
-    print ("#add_field:"+fld+",N,"+fname)
+    print ("#add_field:"+fld+",N,"+fname+","+str(nrow))
