9083cef68171262389f0925fcc9fd93c4192c1dc
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-07 13:47:43 +0300

    compatibility with output agents

--- target_agent_templates/xgboost prediction [logloss].py
+++ target_agent_templates/xgboost prediction [logloss].py
@@ -1,8 +1,8 @@
 #start_of_genes_definitions
 #key=max_depth;  type=random_int;  from=2;  to=12;  step=2
 #key=eta;  type=random_float;  from=0.003;  to=0.03;  step=0.0015
-#key=fields_to_use;  type=random_int;  from=20;  to=300;  step=1
-#key=data;  type=random_array_of_fields;  length=300
+#key=fields_to_use;  type=random_int;  from=20;  to=100;  step=1
+#key=data;  type=random_array_of_fields;  length=100
 #key=colsample_bytree;  type=random_float;  from=0.4;  to=1;  step=0.05
 #key=subsample;  type=random_float;  from=0.4;  to=1;  step=0.05
 #end_of_genes_definitions
@@ -13,6 +13,14 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 import xgboost as xgb
 import math
+
+def my_log_loss(a, b):
+    eps = 1e-9
+    sum1 = 0.0
+    for k in range(0, len(a)):
+        bx = min(max(b[k],eps), 1-eps)
+        sum1 += 1.0 * a[k] * math.log(bx) + 1.0 * (1 - a[k]) * math.log(1 - bx)
+    return -sum1/len(a)
 
 data_defs = {data}
 
@@ -38,41 +46,47 @@ print ("data loaded", len(df), "rows; ", len(df.columns), "columns")
 
 param = {'max_depth':{max_depth}, 'eta':{eta}, 'colsample_bytree':{colsample_bytree}, 'subsample': {subsample}, 'objective':'binary:logistic', 'eval_metric':'logloss', 'nthread':4}
 
-x_test = df.copy(deep=True).iloc[1::2]
-x_test = x_test[x_test[target].notnull()]
-x_test.reset_index(drop=True, inplace=True)
-x_train = df.copy(deep=True).iloc[0::2]
-x_train = x_train[x_train[target].notnull()]
-x_train.reset_index(drop=True, inplace=True)
-
+if not output_mode:
+    x_test = df.copy(deep=True).iloc[1::2]
+    x_test = x_test[x_test[target].notnull()]
+    x_test.reset_index(drop=True, inplace=True)
+    x_train = df.copy(deep=True).iloc[0::2]
+    x_train = x_train[x_train[target].notnull()]
+    x_train.reset_index(drop=True, inplace=True)
+    
+    y_test = x_test[target]
+    
+    num_round = 100000
+else:
+    x_test = df[df[target].isnull()]
+    x_test.reset_index(drop=True, inplace=True)
+    x_train = df[df[target].notnull()]
+    x_train.reset_index(drop=True, inplace=True)
+    
+    num_round = default_num_round
+    
 print ("x_test rows count: " + str(len(x_test)))
 print ("x_train rows count: " + str(len(x_train)))
 
 y_train = x_train[target]
 x_train = x_train.drop(target, 1)
 
-y_test = x_test[target]
 x_test = x_test.drop(target, 1)
 
 dtrain = xgb.DMatrix( x_train, label=y_train)
 dtest = xgb.DMatrix( x_test)
 
-num_round=100000
-watchlist  = [(dtrain,'train'), (xgb.DMatrix( x_test, label=y_test), 'test')]
+if not output_mode:
+	watchlist  = [(dtrain,'train'), (xgb.DMatrix( x_test, label=y_test), 'test')]
+else:
+    watchlist  = [(dtrain,'train')]
+    
 predictor = xgb.train( param, dtrain, num_round, watchlist, verbose_eval = 100, early_stopping_rounds=100 )
 
 prediction = predictor.predict(dtest)
 
-
-def my_log_loss(a, b):
-    eps = 1e-9
-    sum1 = 0.0
-    for k in range(0, len(a)):
-        bx = min(max(b[k],eps), 1-eps)
-        sum1 += 1.0 * a[k] * math.log(bx) + 1.0 * (1 - a[k]) * math.log(1 - bx)
-    return -sum1/len(a)
-
-result = my_log_loss(y_test, prediction)
-
-print ("fitness="+str(result))
+if not output_mode:
+	result = my_log_loss(y_test, prediction)
+	print ("fitness="+str(result))
+    
 
