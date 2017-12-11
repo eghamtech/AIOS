04f11acf4760f1b0849e5e6c9b19cb793f4e0832
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-11 13:36:08 +0300

    out agent

---
+++ output_agent_templates/get_value_at.py
@@ -0,0 +1,8 @@
+#start_of_parameters
+#key=fieldname;  type=constant;  value=enter_fieldname
+#key=filename;  type=constant;  value=enter_filename
+#key=row_number;  type=constant;  value=enter_row_number
+#end_of_parameters
+import pandas as pd
+df = pd.read_csv(workdir + "{filename}")
+print (df.at[{rownumber}, "{fieldname}"])
