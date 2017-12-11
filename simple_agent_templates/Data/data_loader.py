8de6f53da5a9d1b45a113b0ea8de65bd9abaa92d
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-11 13:32:09 +0300

    detect changes

--- simple_agent_templates/Data/data_loader.py
+++ simple_agent_templates/Data/data_loader.py
@@ -11,37 +11,49 @@ source_filename = "{source_filename}"
 target = "{target}"
 
 ####################################################
+print ("start")
 
 newfilename = trainfile
 import pandas as pd
 df = pd.read_csv(workdir+source_filename)
 
-char_cols = list(df.select_dtypes(include=['object']).columns)
-print (char_cols)
 
-def make_dict(col):
-	a1 = col.unique()
-	a1 = [x for x in a1 if str(x) != 'nan']
-	keys1 = range(1, len(a1)+1)
-	return dict(zip(a1, keys1))
+import os.path
+changes = True
+if os.path.isfile(workdir+newfilename):
+    df_old = pd.read_csv(workdir+newfilename)
+    nrow_old = len(df_old.index)
+    print (nrow_old, len(df))
+    if len(df)==nrow_old:
+        changes = False
+
+if changes:
+    char_cols = list(df.select_dtypes(include=['object']).columns)
+    print (char_cols)
+
+    def make_dict(col):
+        a1 = col.unique()
+        a1 = [x for x in a1 if str(x) != 'nan']
+        keys1 = range(1, len(a1)+1)
+        return dict(zip(a1, keys1))
 
 
-for cname in char_cols:
-	dict1 = make_dict(df[cname].fillna(''))
-	df[cname] = df[cname].fillna('').map(dict1)
-	pd.DataFrame(list(dict1.items()), columns=['value', 'key'])[['key','value']].to_csv(workdir+'dict_'+cname+'.csv')    #save new column dict
+    for cname in char_cols:
+        dict1 = make_dict(df[cname].fillna(''))
+        df[cname] = df[cname].fillna('').map(dict1)
+        pd.DataFrame(list(dict1.items()), columns=['value', 'key'])[['key','value']].to_csv(workdir+'dict_'+cname+'.csv')    #save new column dict
 
-df.to_csv(workdir+newfilename, index=False)
+    df.to_csv(workdir+newfilename, index=False)
 
-nrow = len(df)
+    nrow = len(df)
 
-for cname in df.columns:
-	if cname in char_cols:
-		is_dict="Y"
-	else:
-		is_dict="N"
-	if cname==target:
-		is_target="Y"
-	else:
-		is_target="N"
-	print ("#add_field:"+cname+","+is_dict+","+newfilename+","+is_target+","+str(nrow))
+    for cname in df.columns:
+        if cname in char_cols:
+            is_dict="Y"
+        else:
+            is_dict="N"
+        if cname==target:
+            is_target="Y"
+        else:
+            is_target="N"
+        print ("#add_field:"+cname+","+is_dict+","+newfilename+","+is_target+","+str(nrow))
