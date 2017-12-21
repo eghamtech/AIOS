e9ca6f7f771e620fbfba220e45a6793ca455bf7b
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-21 20:28:33 +0300

    OOP version

--- simple_agent_templates/Strings/math_symbols block.py
+++ simple_agent_templates/Strings/math_symbols block.py
@@ -1,47 +1,50 @@
-import pandas as pd
+if 'dicts' not in globals():
+    dicts = {}
 
-col_definition1 = "{random_dict}"
-col1 = col_definition1.split("|")[0]
-file1 = col_definition1.split("|")[1]
+class cls_agent_{id}:
+    import warnings
+    warnings.filterwarnings("ignore")
+    import pandas as pd
 
-result_id = {id}
+    col_definition1 = "{random_dict}"
+    col1 = col_definition1.split("|")[0]
+    file1 = col_definition1.split("|")[1]
+    result_id = {id}
+    field_prefix = 'msym_'
+    symbols = '*+-/%#@^!'
 
-field_prefix = 'msym_'
+    def __init__(self):
+        global dicts
+        
+        self.df = self.pd.read_csv(workdir+self.file1)[[self.col1]]
+        self.fldprefix = self.field_prefix + str(self.result_id)
 
-df = pd.read_csv(workdir+file1)[[col1]]
+    def run_on(self, df_run):
+        if self.col1 not in dicts:
+            self.dict1 = self.pd.read_csv(workdir+'dict_'+self.col1+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
+        else:
+            self.dict1 = {v:k for k,v in dicts[self.col1].items()} # make key=number, value=string
+        
+        df1 = df_run[self.col1].map(self.dict1)
+        idx = 0
+        for c in self.symbols:
+            idx+=1
+            df_run.loc[:,self.fldprefix + '_' + str(idx)] = df1.apply(lambda x: (c in set(x))+0 if type(x)==str else 0)
+    
+    def run(self, mode):
+        print ("enter run mode " + str(mode))
+        self.run_on(self.df)
+        
+        total_cols = len(self.symbols)
+        nrow = len(self.df)
+        
+        for i in range(1,total_cols+1):
+            fld = self.fldprefix + '_' + str(i)
+            fname = fld + '.csv'
+            self.df[[fld]].to_csv(workdir+fname)
+            print ("#add_field:"+fld+",N,"+fname+","+str(nrow))
 
-dict1 = pd.read_csv(workdir+'dict_'+col1+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
-
-df[col1] = df[col1].map(dict1)
-
-fldprefix = field_prefix + str(result_id)
-
-
-df[fldprefix + '_1'] = df[col1].apply(lambda x: ('*' in set(x))+0 if type(type)==str else 0)
-print (1)
-df[fldprefix + '_2'] = df[col1].apply(lambda x: ('+' in set(x))+0 if type(type)==str else 0 )
-print (2)
-df[fldprefix + '_3'] = df[col1].apply(lambda x: ('-' in set(x))+0 if type(type)==str else 0 )
-print (3)
-df[fldprefix + '_4'] = df[col1].apply(lambda x: ('/' in set(x))+0 if type(type)==str else 0 )
-print (4)
-df[fldprefix + '_5'] = df[col1].apply(lambda x: ('%' in set(x))+0 if type(type)==str else 0 )
-print (5)
-df[fldprefix + '_6'] = df[col1].apply(lambda x: ('#' in set(x))+0 if type(type)==str else 0 )
-print (6)
-df[fldprefix + '_7'] = df[col1].apply(lambda x: ('@' in set(x))+0 if type(type)==str else 0 )
-print (7)
-df[fldprefix + '_8'] = df[col1].apply(lambda x: ('^' in set(x))+0 if type(type)==str else 0 )
-print (8)
-df[fldprefix + '_9'] = df[col1].apply(lambda x: ('!' in set(x))+0 if type(type)==str else 0 )
-print (9)
-
-total_cols = 9
-
-nrow = len(df)
-
-for i in range(1,total_cols+1):
-    fld = fldprefix + '_' + str(i)
-    fname = fld + '.csv'
-    df[[fld]].to_csv(workdir+fname)
-    print ("#add_field:"+fld+",N,"+fname+","+str(nrow))
+    def apply(self, df_add):
+        self.run_on(df_add)
+    
+agent_{id} = cls_agent_{id}()
