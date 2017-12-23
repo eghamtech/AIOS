a2ccbfcb9bf40da3dc4e8386a55a59edd01e32d7
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-23 14:44:31 +0300

    OOP version

--- simple_agent_templates/Strings/word_match_share.py
+++ simple_agent_templates/Strings/word_match_share.py
@@ -1,86 +1,104 @@
-import pandas as pd
-import nltk.corpus
-import string
-stopwords = nltk.corpus.stopwords.words('english')
-stopwords.extend(string.punctuation)
-stopwords.append('')
-stopwords.append('would')
+if 'dicts' not in globals():
+    dicts = {}
 
-col_definition1 = "{random_dict}"
-col1 = col_definition1.split("|")[0]
-file1 = col_definition1.split("|")[1]
-col_definition2 = "{random_dict}"
-col2 = col_definition2.split("|")[0]
-file2 = col_definition2.split("|")[1]
-
-result_id = {id}
-
-field_prefix = "field_"
-output_filename = field_prefix + str(result_id) + ".csv"
-
-df = pd.read_csv(workdir+file1)[[col1]]
-df = df.merge(pd.read_csv(workdir+file2)[[col2]], left_index=True, right_index=True)
-
-dict1 = pd.read_csv(workdir+'dict_'+col1+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
-dict2 = pd.read_csv(workdir+'dict_'+col2+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
-
-df[col1] = df[col1].map(dict1)
-df[col2] = df[col2].map(dict2)
-
-
-def word_match_share(s1, s2):
-    q1words = {}
-    q2words = {}
-    for word in s1.split():
-        if word not in stopwords:
-            q1words[word] = 1
-    for word in s2.split():
-        if word not in stopwords:
-            q2words[word] = 1
-    if len(q1words) == 0 or len(q2words) == 0:
-        return 0
-    shared_words_in_q1 = [w for w in q1words.keys() if w in q2words]
-    shared_words_in_q2 = [w for w in q2words.keys() if w in q1words]
-    R = (len(shared_words_in_q1) + len(shared_words_in_q2))/(len(q1words) + len(q2words))
-    return R
-
-func = lambda s: s[:1].lower() + s[1:] if s else ''
-
-
-
-fldprefix = field_prefix + str(result_id)
-
-block = int(len(df)/50)
-i = block-1
-
-for index, row in df.iterrows():
-    i+=1
-    if type(row[col1])==str:
-        sline1 = func(row[col1])
-    else:
-        sline1 = ''
-    if type(row[col2])==str:
-        sline2 = func(row[col2])
-    else:
-        sline2 = ''
+class cls_agent_{id}:
+    import warnings
+    warnings.filterwarnings("ignore")
     
-    df.set_value(index, fldprefix + '_1', word_match_share(sline1, sline2))
-    
-    if i>=block:
-        i=0
-        print (index)
+    import pandas as pd
+    import nltk.corpus
+    import string
+    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords.extend(string.punctuation)
+    stopwords.append('')
+    stopwords.append('would')
 
+    col_definition1 = "{random_dict}"
+    col1 = col_definition1.split("|")[0]
+    file1 = col_definition1.split("|")[1]
+    col_definition2 = "{random_dict}"
+    col2 = col_definition2.split("|")[0]
+    file2 = col_definition2.split("|")[1]
+    result_id = {id}
+    field_prefix = "field_"
+    output_filename = field_prefix + str(result_id) + ".csv"
+
+    def func(self, s):
+        return s[:1].lower() + s[1:] if s else ''
+    
+    def __init__(self):
+        self.fldprefix = self.field_prefix + str(self.result_id)
+
+    def word_match_share(self, s1, s2):
+        q1words = {}
+        q2words = {}
+        for word in s1.split():
+            if word not in self.stopwords:
+                q1words[word] = 1
+        for word in s2.split():
+            if word not in self.stopwords:
+                q2words[word] = 1
+        if len(q1words) == 0 or len(q2words) == 0:
+            return 0
+        shared_words_in_q1 = [w for w in q1words.keys() if w in q2words]
+        shared_words_in_q2 = [w for w in q2words.keys() if w in q1words]
+        R = (len(shared_words_in_q1) + len(shared_words_in_q2))/(len(q1words) + len(q2words))
+        return R
         
+    def run_on(self, df_run):
+        if self.col1 not in dicts:
+            self.dict1 = self.pd.read_csv(workdir+'dict_'+self.col1+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
+        else:
+            self.dict1 = {v:k for k,v in dicts[self.col1].items()} # make key=number, value=string
+        if self.col2 not in dicts:
+            self.dict2 = self.pd.read_csv(workdir+'dict_'+self.col2+'.csv', dtype={'value': object}).set_index('key')["value"].to_dict()
+        else:
+            self.dict2 = {v:k for k,v in dicts[self.col2].items()} # make key=number, value=string
+            
+        self.dfx = self.pd.DataFrame()
+        self.dfx[self.col1] = df_run[self.col1].map(self.dict1)
+        self.dfx[self.col2] = df_run[self.col2].map(self.dict2)
         
-newfields = []
+        block = int(len(df_run)/50)
+        i = 0
 
-for i in range(1,2):
-    newfields.append(fldprefix + '_' + str(i))
-    
-df[newfields].to_csv(workdir+output_filename)
+        for index, row in self.dfx.iterrows():
+            i+=1
+            if type(row[self.col1])==str:
+                sline1 = self.func(row[self.col1])
+            else:
+                sline1 = ''
+            if type(row[self.col2])==str:
+                sline2 = self.func(row[self.col2])
+            else:
+                sline2 = ''
+        
+            df_run.set_value(index, self.fldprefix + '_1', self.word_match_share(sline1, sline2))
 
-nrow = len(df)
+            if i>=block and block>=1000:
+                i=0
+                print (index)
+                
+    def run(self, mode):
+        print ("enter run mode " + str(mode))
+        self.df = self.pd.read_csv(workdir+self.file1)[[self.col1]]
+        self.df = self.df.merge(self.pd.read_csv(workdir+self.file2)[[self.col2]], left_index=True, right_index=True)
+        
+        self.run_on(self.df)
+        
+        newfields = []
 
-for fld in newfields:
-    print ("#add_field:"+fld+",N,"+output_filename+","+str(nrow))
+        for i in range(1,2):
+            newfields.append(self.fldprefix + '_' + str(i))
 
+        self.df[newfields].to_csv(workdir+self.output_filename)
+
+        nrow = len(self.df)
+
+        for fld in newfields:
+            print ("#add_field:"+fld+",N,"+self.output_filename+","+str(nrow))
+
+    def apply(self, df_add):
+        self.run_on(df_add)
+
+agent_{id} = cls_agent_{id}()
