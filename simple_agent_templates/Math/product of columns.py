df73c867162f2ab6294bdba45ededeedf17f1c9b
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-24 18:23:14 +0300

    OOP version

--- simple_agent_templates/Math/product of columns.py
+++ simple_agent_templates/Math/product of columns.py
@@ -1,23 +1,36 @@
-import pandas as pd
+class cls_agent_{id}:
+    import warnings
+    warnings.filterwarnings("ignore")
+    
+    import pandas as pd
 
-col_definition1 = "{random_field_numeric}"
-col1 = col_definition1.split("|")[0]
-file1 = col_definition1.split("|")[1]
-col_definition2 = "{random_field_numeric}"
-col2 = col_definition2.split("|")[0]
-file2 = col_definition2.split("|")[1]
+    col_definition1 = "{random_field_numeric}"
+    col1 = col_definition1.split("|")[0]
+    file1 = col_definition1.split("|")[1]
+    col_definition2 = "{random_field_numeric}"
+    col2 = col_definition2.split("|")[0]
+    file2 = col_definition2.split("|")[1]
 
-result_id = {id}
-field_prefix = "field_"
-output_column = field_prefix + str(result_id)
-output_filename = output_column + ".csv"
+    result_id = {id}
+    field_prefix = "field_"
+    output_column = field_prefix + str(result_id)
+    output_filename = output_column + ".csv"
 
-df = pd.read_csv(workdir+file1)[[col1]]
-df = df.merge(pd.read_csv(workdir+file2)[[col2]], left_index=True, right_index=True)
+    def run_on(self, df_run):
+        df_run[self.output_column] = df_run[self.col1] * df_run[self.col2]
 
-df[output_column] = df[col1] * df[col2]
-df[[output_column]].to_csv(workdir+output_filename)
+    def run(self, mode):
+        print ("enter run mode " + str(mode))
+        self.df = self.pd.read_csv(workdir+self.file1)[[self.col1]]
+        self.df = self.df.merge(self.pd.read_csv(workdir+self.file2)[[self.col2]], left_index=True, right_index=True)
+        
+        self.run_on(self.df)
+        
+        self.df[[self.output_column]].to_csv(workdir+self.output_filename)
+        print (self.col1+"*"+self.col2)
+        print ("#add_field:"+self.output_column+",N,"+self.output_filename+","+str(len(self.df)))
+    
+    def apply(self, df_add):
+        self.run_on(df_add)
 
-print (col1+"*"+col2)
-print ("#add_field:"+output_column+",N,"+output_filename+","+str(len(df)))
-
+agent_{id} = cls_agent_{id}()
