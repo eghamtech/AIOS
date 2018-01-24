2d1add73ea376afee243b969a8181c384c1d4ca7
direct1man <33375789+direct1man@users.noreply.github.com>
2018-01-24 20:44:13 +0300

    fix

--- simple_agent_templates/Math/standard scaler.py
+++ simple_agent_templates/Math/standard scaler.py
@@ -31,28 +31,33 @@ class cls_agent_{id}:
     sfx = ["_ss_", "_mm_", "_qt_"]
     scalers_loaded = False
     scalers = []
+    
+    # "workdir" must be specified in Constants - it is a global setting where all CSV files are stored on Jupyter server
+    # read the data for selected column
+    df = pd.read_csv(workdir+file1)[[col1]]
+    # convert selected field to Numpy Array first and reshape as required by sklearn
+    np_base_column = np.array(df[col1]).reshape(-1, 1)
+    # find max and min values ignoring NaN and INF values
+    np_column_max = np.ma.masked_invalid(np_base_column).max()
+    np_column_min = np.ma.masked_invalid(np_base_column).min() 
+    # replace INF and -INF with above
+    np_base_column[np_base_column == inf] = np_column_max
+    np_base_column[np_base_column == -inf] = np_column_min
+
+    # replace NaN with mean values
+    imp = Imputer()
+    imp.fit(np_base_column)
 
     def run(self, mode):
         print ("enter run mode " + str(mode))
-        # "workdir" must be specified in Constants - it is a global setting where all CSV files are stored on Jupyter server
-        # read the data for selected column
-        df = self.pd.read_csv(workdir+self.file1)[[self.col1]]
-        nrow = len(df)
+        
+        nrow = len(self.df)
 
         #print( self.np.argwhere(self.np.isnan(self.np.array(df))) )
         #print( self.np.argwhere(self.np.isinf(self.np.array(df))) )
 
-        # convert selected field to Numpy Array first and reshape as required by sklearn
-        np_column = self.np.array(df[self.col1]).reshape(-1, 1)
-        # find max and min values ignoring NaN and INF values
-        np_column_max = self.np.ma.masked_invalid(np_column).max()
-        np_column_min = self.np.ma.masked_invalid(np_column).min() 
-        # replace INF and -INF with above
-        np_column[np_column == self.inf] = np_column_max
-        np_column[np_column == -self.inf] = np_column_min
-
         # replace NaN with mean values
-        np_column = self.Imputer().fit_transform(np_column)
+        np_column = self.imp.transform(self.np_base_column)
 
         for i in range(1,4):
             # create new field name with unique instance ID
@@ -67,8 +72,8 @@ class cls_agent_{id}:
             elif i==3:
                 scaler = self.QuantileTransformer(n_quantiles=100)
                 
-            df[output_column] = scaler.fit_transform(np_column)
-            df[[output_column]].to_csv(workdir+output_filename)
+            self.df[output_column] = scaler.fit_transform(np_column)
+            self.df[[output_column]].to_csv(workdir+output_filename)
         
             print ("Scaler" + str(i) + "("+self.col1+")")
             print ("#add_field:"+output_column+",N,"+output_filename+","+str(nrow))
@@ -79,11 +84,9 @@ class cls_agent_{id}:
     
     def apply(self, df_add):
         np_column = self.np.array(df_add[self.col1]).reshape(-1, 1)
-        np_column_max = self.np.ma.masked_invalid(np_column).max()
-        np_column_min = self.np.ma.masked_invalid(np_column).min() 
-        np_column[np_column == self.inf] = np_column_max
-        np_column[np_column == -self.inf] = np_column_min
-        np_column = self.Imputer().fit_transform(np_column)
+        np_column[np_column == self.inf] = self.np_column_max
+        np_column[np_column == -self.inf] = self.np_column_min
+        np_column = self.imp.transform(np_column)
 
         for i in range(1,4):
             output_column = "scaled_" + self.col1 + self.sfx[i-1] + str(self.result_id)
