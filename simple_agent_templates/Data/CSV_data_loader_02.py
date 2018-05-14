97617dbacc503a97b28eba3a367b8727a30bf864
Aichoo AIOS <ds@eghamtech.com>
2018-05-14 18:39:08 +0100

    v0

--- simple_agent_templates/Data/CSV_data_loader_02.py
+++ simple_agent_templates/Data/CSV_data_loader_02.py
@@ -1,5 +1,8 @@
 #start_of_parameters
 #key=source_filename;  type=constant;  value=enter_source_filename
+#key=source_primary_field;  type=constant;  value=enter_source_primary_field
+#key=primary_field;  type=constant;  value=enter_primary_field|its_filename
+#key=field_prefix;  type=constant;  value=csv02
 #key=target;  type=constant;  value=enter_target
 #end_of_parameters
 
@@ -8,6 +11,9 @@
 # Provide correct 'source_filename' in the agent parameters.
 # Variables 'workdir' and 'trainfile' (target filename) must be setup in 'Constants' area of AIOS
 # Parameter 'target' specifies column to be marked as the prediction target.
+#
+# this version of the loader will scan existing data and will append new columns from "source_filename"
+# where "source_primary_field" == "primary_field" column already in the AIOS Memory
 
 if 'dicts' not in globals():
     dicts = {}  # dict of dicts. each of dicts has structure: key=string, value=number
@@ -16,17 +22,39 @@ class cls_agent_{id}:
     import warnings
     warnings.filterwarnings("ignore")
     import pandas as pd
+    import re
     
     source_filename = "{source_filename}"
+    source_primary_field = "{source_primary_field}"
+    primary_field = "{primary_field}"
+    new_field_prefix = "{field_prefix}"
     target = "{target}"
-    newfilename = trainfile
+   
+    # obtain a unique ID for the current instance
+    result_id = {id}
+    # create new field name based on "new_field_prefix" with unique instance ID
+    # and filename to save new field data
+    output_column = new_field_prefix + "_" + str(result_id)
+    output_filename = output_column + ".csv"
     
+    colmap = {}
+        
     def __init__(self):
         global dicts
         self.df = self.pd.read_csv(workdir+self.source_filename, encoding='utf8')
-        #print (self.df)
+        
+        new_cols = []
+        for c in self.df.columns:
+              str1 = c
+              str1 = self.re.sub('[^0-9a-zA-Z]+', '_', str1)
+              str1 = output_column + "_" + str1
+              new_cols.append(str1)
+              self.colmap[c] = str1
+        df.columns = new_cols
+        
         self.char_cols = list(self.df.select_dtypes(include=['object']).columns)
         print ("source data loaded")
+        print ("rows: ", len(self.df))
         print ("char columns:", self.char_cols)
         #self.dicts = {}
         for cname in self.char_cols:
@@ -41,6 +69,12 @@ class cls_agent_{id}:
     def run(self, mode):
         global dicts
         print ("enter run mode " + str(mode))
+        
+        col_name = self.primary_field.split("|")[0]
+        file_name = self.primary_field.split("|")[1]
+        df_primary = self.pd.read_csv(workdir+file_name, encoding='utf8')[[col_name]]
+        df_primary.merge
+    
         for cname in self.char_cols:
             dict1 = dicts[cname]
             self.df[cname] = self.df[cname].fillna('').map(dict1)
