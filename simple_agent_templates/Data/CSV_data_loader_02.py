3f6a5a268dde3d056d5b5eceb1113e46f52c1cd8
Aichoo AIOS <ds@eghamtech.com>
2018-07-09 14:52:15 +0100

    saving model

--- simple_agent_templates/Data/CSV_data_loader_02.py
+++ simple_agent_templates/Data/CSV_data_loader_02.py
@@ -22,7 +22,7 @@ class cls_agent_{id}:
     import warnings
     warnings.filterwarnings("ignore")
     import pandas as pd
-    import re
+    import re, bz2, pickle, os.path
     
     source_filename = "{source_filename}"                  # file in workdir where additional data is
     source_primary_field = "{source_primary_field}"        # name of the field which can be used to link to existing data
@@ -32,37 +32,25 @@ class cls_agent_{id}:
    
     # obtain a unique ID for the current instance
     result_id = {id}
+    agent_name = 'csv_agent_' + str(result_id)
     # create new field name based on "new_field_prefix" with unique instance ID
     # and filename to save new field data
-    output_column = new_field_prefix + "_" + str(result_id)
-    output_filename = output_column + ".csv"
+    # output_column = new_field_prefix + "_" + str(result_id)
+    # output_filename = output_column + ".csv"
     
     colmap = {}
+    char_cols = []
         
     def __init__(self):
         global dicts
-        self.df = self.pd.read_csv(workdir+self.source_filename, encoding='utf8')
-        
-        new_cols = []
-        for c in self.df.columns:
-              str1 = c
-              str1 = self.re.sub('[^0-9a-zA-Z]+', '_', str1)
-              str1 = self.output_column + "_" + str1
-              new_cols.append(str1)
-              self.colmap[c] = str1
-        self.df.columns = new_cols
-        
-        # create a copy of specified primary field to have the same name as primary field in AIOS Memory
-        # so dataframe can be joined on that field
-        self.df[self.primary_field] = self.df[self.colmap[self.source_primary_field]]
-        
-        self.char_cols = list(self.df.select_dtypes(include=['object']).columns)
-        print ("source data loaded")
-        print ("rows: ", len(self.df))
-        print ("char columns:", self.char_cols)
-        #self.dicts = {}
-        for cname in self.char_cols:
-            dicts[cname] = self.make_dict(self.df[cname].fillna(''))
+        # if saved model for dictionaries already exists then load it from filesystem
+        if self.os.path.isfile(workdir + self.agent_name + '.model'):
+            rfile = self.bz2.BZ2File(workdir + self.agent_name + '.model', 'r')
+            dicts = self.pickle.load(rfile)
+            rfile.close()
+            
+            self.colmap = dicts[self.agent_name + '.colmap']
+            self.char_cols = dicts[self.agent_name + '.char_cols']
 
     def make_dict(self, col):
         a1 = col.unique()
@@ -71,44 +59,99 @@ class cls_agent_{id}:
         return dict(zip(a1, keys1))
 
     def run(self, mode):
+        from datetime import datetime
         global dicts
         print ("enter run mode " + str(mode))
+        print (str(datetime.now()), " creating dataframe...")
+        self.df = self.pd.read_csv(workdir+self.source_filename, encoding='utf8')
+        
+        new_cols = []
+        self.colmap = {}
+        for c in self.df.columns:
+              str1 = c
+              str1 = self.re.sub('[^0-9a-zA-Z]+', '_', str1)
+              str1 = new_field_prefix + "_" + str1 + "_" + str(self.result_id)
+              new_cols.append(str1)                               # list of new columns
+              self.colmap[c] = str1                               # a map from old column names to new ones
+        self.df.columns = new_cols                                # assign new column names to the dataframe
+        
+        # create a copy of specified primary field to have the same name as primary field in AIOS Memory
+        # so dataframe can be joined on that field
+        self.df[self.primary_field] = self.df[self.colmap[self.source_primary_field]]
         
         col_name = self.primary_field.split("|")[0]
         file_name = self.primary_field.split("|")[1]
         df_primary = self.pd.read_csv(workdir+file_name, encoding='utf8')[[col_name]]
+        
         df_primary = self.pd.merge(df_primary, self.df, how='left', on=self.primary_field, sort=False)
         df_primary.drop(self.primary_field, axis=1)              # remove primary field as it is a duplicate previously created
-    
+        
+        print (str(datetime.now()), " processing TEXT columns")
+        self.char_cols = list(df_primary.select_dtypes(include=['object']).columns)
+        print ("source data loaded")
+        print ("rows: ", len(df_primary))
+ 
         for cname in self.char_cols:
-            dict1 = dicts[cname]
-            self.df_primary[cname] = self.df_primary[cname].fillna('').map(dict1)
-            self.pd.DataFrame(list(dict1.items()), columns=['value', 'key'])[['key','value']].to_csv(workdir+'dict_'+cname+'.csv', encoding='utf-8')    #save new column dict
+            dict_char = self.make_dict(df_primary[cname].fillna(''))
+            dicts[cname] = dict_char
+            df_primary[cname] = df_primary[cname].fillna('').map(dict_char)
+            # save dictionary for each text column into separate file
+            self.pd.DataFrame(list(dict_char.items()), columns=['value', 'key'])[['key','value']].to_csv(workdir+'dict_'+cname+'.csv', encoding='utf-8')
+            print ("text column: " + cname + " processed")
         
-        self.df_primary.to_csv(workdir+self.output_filename, index=False)
+        print (str(datetime.now()), " saving dicts...")           
+        dicts[self.agent_name + '.colmap'] = self.colmap
+        dicts[self.agent_name + '.char_cols'] = self.char_cols
+       
+        sfile = self.bz2.BZ2File(workdir + self.agent_name + '.model', 'w')
+        self.pickle.dump(dicts, sfile) 
+        sfile.close()
         
-        nrow = len(self.df_primary)
-
-        for cname in self.df_primary.columns:
+        print (str(datetime.now()), " ...dicts saved.")
+        
+        # self.df_primary.to_csv(workdir+self.output_filename, index=False) - deprecated
+        
+        nrow = len(df_primary)
+        is_use_for_models="Y"
+        for cname in df_primary.columns:
             if cname in self.char_cols:
                 is_dict="Y"
             else:
                 is_dict="N"
             if cname==self.target:
                 is_target="Y"
+                is_use_for_models="N"
             else:
                 is_target="N"
-            print ("#add_field:"+cname+","+is_dict+","+self.output_filename+","+is_target+","+str(nrow))
+                
+            # save each column into separate file and register new field
+            output_column = cname
+            output_filename = output_column + ".csv"
+            df_primary[[output_column]].to_csv(workdir+output_filename)           
+            print ("#add_field:"+output_column+","+is_dict+","+output_filename+","+is_target+","+str(nrow)+","+is_use_for_models)
+           
     
     def apply(self, df_add):
+       # this function is called when new data needs to be processed
+        # new data supplied in df_add
         global dicts
-        for index, row in df_add.iterrows():
+        
+        # creal - original column name; cshort - converted column name
+        # check which of the fields originally processed by this class present in df_add and 
+        # create missing ones with NaN or rename as previously done
+        for creal, cshort in self.colmap.items():
+            if creal not in df_add.columns:
+                df_add[cshort] = float('nan')
+            else:
+                df_add.rename(index=str, columns={creal: cshort}, inplace=True)
+                
+        for index, row in df_add.iterrows():                                     # iterate over each row in df_add
             for cname in df_add.columns:
                 if cname in self.char_cols:
-                    if not (row[cname] in dicts[cname]):
-                        dicts[cname][row[cname]] = 1+max(dicts[cname].values())
+                    if not (row[cname] in dicts[cname]):                         # if value in current row and column not in dictionary
+                        dicts[cname][row[cname]] = 1+max(dicts[cname].values())  # create new key in dictionary with max+1 value
                     df_add.at[index, cname] = dicts[cname][row[cname]]
-                else:
-                    df_add.at[index, cname] = row[cname]
+                #else:
+                #    df_add.at[index, cname] = row[cname]
 
 agent_{id} = cls_agent_{id}()
