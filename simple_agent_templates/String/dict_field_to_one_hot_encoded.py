2c6a2f1d7c13c69da587bbe6bb22bf532b9c70eb
Aichoo AIOS <ds@eghamtech.com>
2019-03-07 01:10:18 +0000

    Update dict_field_to_one_hot_encoded.py

--- simple_agent_templates/String/dict_field_to_one_hot_encoded.py
+++ simple_agent_templates/String/dict_field_to_one_hot_encoded.py
@@ -20,7 +20,7 @@ class cls_agent_{id}:
     warnings.filterwarnings("ignore")
     
     import pandas as pd
-    import os.path
+    import os.path, bz2, pickle
     
     col_definition1 = "{random_field_distinct}"
     col1  = col_definition1.split("|")[0]
@@ -31,17 +31,17 @@ class cls_agent_{id}:
     # create new field name based on "new_field_prefix" with unique instance ID
     # and filename to save new field data
     new_field_prefix  = "{new_field_prefix}"
-	max_unique_values = {max_unique_values}
-	col_max_length    = {col_max_length}
-	
-	dicts_agent = {}
-	new_columns = []
+    max_unique_values = {max_unique_values}
+    col_max_length    = {col_max_length}
+
+    dicts_agent = {}
+    new_columns = []
 	
     def is_set(self, s):
         return len(s)>0 and s!="0"
     
     def __init__(self):
-		from datetime import datetime
+	from datetime import datetime
         # if saved dictionaries for the target field already exist then load them from filesystem              
         if self.os.path.isfile(workdir + self.col1 + '_dicts.model'):
             rfile = self.bz2.BZ2File(workdir + self.col1 + '_dicts.model', 'r')
@@ -50,65 +50,65 @@ class cls_agent_{id}:
             print (str(datetime.now()), self.col1 + ': one hot encoding agent dictionaries model loaded')
 
     def run_on(self, df_run):
-		col_name = self.col1
-		self.new_columns = []
-		
-		for k,v in self.dicts_agent[col_name].items():
-			new_col_name = self.new_field_prefix + col_name + '_' + str(self.result_id) + '_v_' + str(v)
-			new_col_name = new_col_name[:self.col_max_length]
-			self.new_columns.append(new_col_name)
-			df_run[new_col_name] = 0
-		
-		for index, row in df_run.iterrows():
-			value = int(round(row[col_name],0))
-			value_mapped = self.dicts_agent[col_name].get(value)
-			
-			if value_mapped != None:
-				new_col_name = self.new_field_prefix + col_name + '_' + str(self.result_id) + '_v_' + str(value_mapped)
-				new_col_name = new_col_name[:self.col_max_length]
-				df_run.at[index, new_col_name] = 1
+	col_name = self.col1
+	self.new_columns = []
+
+	for k,v in self.dicts_agent[col_name].items():
+	    new_col_name = self.new_field_prefix + col_name + '_' + str(self.result_id) + '_v_' + str(v)
+	    new_col_name = new_col_name[:self.col_max_length]
+	    self.new_columns.append(new_col_name)
+	    df_run[new_col_name] = 0
+
+	for index, row in df_run.iterrows():
+	    value = int(round(row[col_name],0))
+	    value_mapped = self.dicts_agent[col_name].get(value)
+
+	    if value_mapped != None:
+		new_col_name = self.new_field_prefix + col_name + '_' + str(self.result_id) + '_v_' + str(value_mapped)
+		new_col_name = new_col_name[:self.col_max_length]
+		df_run.at[index, new_col_name] = 1
 		
 	
     def run(self, mode):
         print ("enter run mode " + str(mode))
-		self.df  = self.pd.read_csv(workdir+self.file1)[[self.col1]]
+	self.df  = self.pd.read_csv(workdir+self.file1)[[self.col1]]
 
-		self.df[self.col1] = self.df[self.col1].apply(lambda x: int(round(x,0)))
-		unique_list = self.df[self.col1].unique()
+	self.df[self.col1] = self.df[self.col1].apply(lambda x: int(round(x,0)))
+	unique_list = self.df[self.col1].unique()
 		
-		if len(unique_list) == 1:
+	if len(unique_list) == 1:
             print ("Selected column contains only 1 unique value - no point to do anything with it.")
             # register the same field as the source field, which notifies AIOS of successful exit
             # and instructs to mark such field with use_for_models=False
             print ("#add_field:"+self.col1+",N,"+self.file1+","+str(len(self.df))+",N")   
             return    
 		
-		if len(unique_list) > self.max_unique_values:
-			print ("Selected column contains " + str(unique_v) + " unique values which is more than " + str(max_unique_values) + " allowed - ignoring it.")
-			return
+	if len(unique_list) > self.max_unique_values:
+	    print ("Selected column contains " + str(unique_v) + " unique values which is more than " + str(max_unique_values) + " allowed - ignoring it.")
+	    return
 			
-		file_name  = self.file1
-		col_name   = self.col1
-		# load dictionary if it exists
-        if self.os.path.isfile(workdir + 'dict_' + file_name):
-            dict_temp = self.pd.read_csv(workdir + 'dict_' + file_name, dtype={'value': object}).set_index('key')["value"].to_dict()
-		else:
-			dict_temp = {x:str(x) for x in unique_list if str(x) != 'nan'}
+    	file_name  = self.file1
+    	col_name   = self.col1
+    	# load dictionary if it exists
+    	if self.os.path.isfile(workdir + 'dict_' + file_name):
+	    dict_temp = self.pd.read_csv(workdir + 'dict_' + file_name, dtype={'value': object}).set_index('key')["value"].to_dict()
+    	else:
+	    dict_temp = {x:str(x) for x in unique_list if str(x) != 'nan'}
+
+    	self.dicts_agent[col_name] = dict_temp
+    	self.df["dict_"+col_name]  = self.df[col_name].map(dict_temp)
+
+    	self.run_on(self.df)                      
+    	nrow = len(self.df)
 		
-		self.dicts_agent[col_name] = dict_temp
-        self.df["dict_"+col_name]  = self.df[col_name].map(dict_temp)
-        
-		self.run_on(self.df)                      
-        nrow = len(self.df)
-		
-		self.dicts_agent['new_columns'] = self.new_columns
-		# save dictionary of all auxiliary data into file
+	self.dicts_agent['new_columns'] = self.new_columns
+	# save dictionary of all auxiliary data into file
         sfile = self.bz2.BZ2File(workdir + col_name + '_dicts.model', 'w')
         self.pickle.dump(self.dicts_agent, sfile) 
         sfile.close()
 		
-		# save and register each new column
-		for i in range(0,self.new_columns):
+	# save and register each new column
+	for i in range(0,self.new_columns):
             fld   = self.new_columns[i]
             fname = fld + '.csv'
             self.df[[fld]].to_csv(workdir+fname)
